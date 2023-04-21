e3a6fd877c5152c110e89ef6bbc663d1804f84aa
ptdrdheeraj <ptdrdheeraj22@gmail.com>
2023-04-21 15:41:51 +0530

    project4

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rays.pro4.Exception;
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author Dheeraj patidar
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg){
+		super(msg);
+	}
+}
